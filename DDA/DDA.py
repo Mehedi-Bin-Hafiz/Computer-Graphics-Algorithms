1e9be2b34a648e9d134acdfc8571ae867934041f
Mithu12 <mosaddekalimithu123@gmail.com>
2020-10-14 02:42:08 +0600

    Programmes debugged

--- DDA/DDA.py
+++ DDA/DDA.py
@@ -9,20 +9,32 @@ def customround(M):
         M = round(M)
     return float(M)
 
+def fuckYouReality(x):
+    if x < low:
+        print('fuck low', low)
+        return True
+    elif x > high:
+        print('fuck high', high)
+        return True
 
-def DDA(x0,y0,x1,y1):
+
+def DDA(x0, y0, x1, y1):
     x1 = float(x1)
     y1 = float(y1)
-    gx1=list()
-    gy1=list()
-    cordination=list()
-    xn= float(x0)
-    yn= float(y0)
+    gx1 = list()
+    gy1 = list()
+    cordination = list()
+    xn = float(x0)
+    yn = float(y0)
     m = (y1-y0)/(x1-x0)
     m =round(m,2)
 
-    print("The value of m is= ",m)
+    print("The value of m is= ", m)
     if 0 < m and m <= 1:
+        xT = x0 + 1
+        if fuckYouReality(xT):  # asdasdasdadasdasdas
+            x0, x1, xn = x1, x0, x1
+            y0, y1, yn = y1, y0, y1
         gx1.append(xn)
         gy1.append(yn)
         cordination.append((customround(xn), customround(yn)))
@@ -32,34 +44,38 @@ def DDA(x0,y0,x1,y1):
             gx1.append(xn)
             gy1.append(yn)
             cordination.append((customround(xn), customround(yn)))
-            x0, y0 = xn,yn
-            if xn == x1 :
+            x0, y0 = xn, yn
+            if xn >= x1 :
                 break
             else:
                 continue
     elif( -1<= m and m <= 0):
-        if(x0>x1 and y0>y1):
-            temp=xn
-            xn=x1
-            x1=temp
-            temp2=yn
-            yn=y1
-            y1=temp2
+
+        xT = x0 - 1
+        if fuckYouReality(xT): #asdasdasdadasdasdas
+            x0, x1, xn = x1, x0, x1
+            y0, y1, yn = y1, y0, y1
         gx1.append(xn)
         gy1.append(yn)
         cordination.append((customround(xn), customround(yn)))
         while True:
             xn = x0 - 1
             yn = y0 - m
+            print(xn, yn)
+            time.sleep(1)
             gx1.append(xn)
             gy1.append(yn)
             cordination.append((customround(xn), customround(yn)))
             x0, y0 = xn, yn
-            if xn == x1 or yn == y1:
+            if xn <= x1:   # =============== change < and > if infinity
                 break
             else:
                 continue
     elif (m > 1):
+        xT = x0 + (1/m)
+        if fuckYouReality(xT):  # asdasdasdadasdasdas
+            x0, x1, xn = x1, x0, x1
+            y0, y1, yn = y1, y0, y1
         gx1.append(xn)
         gy1.append(yn)
         cordination.append((customround(xn), customround(yn)))
@@ -68,45 +84,55 @@ def DDA(x0,y0,x1,y1):
             y = y0 + 1
             gx1.append(x)
             gy1.append(y)
+            fuckYouReality(x)  # asdasdasdadasdasdas
             cordination.append((customround(x), customround(y)))
             x0, y0 = x, y
-            if x == x1 :
+            if x >= x1:
                 break
             else:
                 continue
 
     elif(m < -1):
-        if (x0 > x1 and y0 > y1):
-            temp = xn
-            xn = x1
-            x1 = temp
-            temp2 = yn
-            yn = y1
-            y1 = temp2
+        xT = x0 - (1 / m)
+        print(xT)
+        print(x1)
+        time.sleep(.2)
+        if fuckYouReality(xT):  # asdasdasdadasdasdas
+            x0, x1, xn = x1, x0, x1
+            y0, y1, yn = y1, y0, y1
         gx1.append(xn)
         gy1.append(yn)
         cordination.append((customround(xn), customround(yn)))
         while True:
             x = x0 - (1 / m)
             y = y0 - 1
+            print(x, y)
+            time.sleep(.5)
             gx1.append(x)
             gy1.append(y)
             cordination.append((customround(x), customround(y)))
             x0, y0 = x, y
-            if xn == x1 or yn == y1:
+            if x >= x1:
                 break
             else:
                 continue
     return gx1, gy1, cordination
 
 
+x1, y1, x2, y2 = 6, 5, 5,  15,
+# low, high = 0, 0
+if(x2 - x1 < 0):
+    low = x2
+    high = x1
+else:
+    low = x1
+    high = x2
 
-xnew1,ynew1, Ncordination = DDA(15,5,5,10)
+xnew1,ynew1, Ncordination = DDA(x1, y1, x2, y2)
 # xnew1,ynew1, Ncordination = DDA(15,27,26,33)
 
-
 print("x1         y1         (x1,y1)")
-for i in range(0,len(xnew1)):
-    print("{:.2f}  -> {:>.2f} ->    {}".format(xnew1[i], ynew1[i],Ncordination[i]))
+for i in range(0, len(xnew1)):
+    print("{:.2f}  -> {:>.2f} ->    {}".format(xnew1[i], ynew1[i], Ncordination[i]))
 
 
