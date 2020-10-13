f641f5a1b75cf600797b34a3117ad5cfd0bf9039
Mithu12 <mosaddekalimithu123@gmail.com>
2020-10-14 00:49:28 +0600

    Bresen Algorithm added

---
+++ BresenHam/Bresen.py
@@ -0,0 +1,28 @@
+x1, y1, x2, y2 = 18, 35, 27, 41
+
+dx = x2-x1
+dy = y2-y1
+p0 = 2*dy - dx
+print('dx : ', dx)
+print('dy : ', dy)
+print('p0 : ', p0)
+a = ' \t'
+print('=========== Co-ords Values============')
+print(' p\t   x\t  y\t     (x,y)')
+print(' {:}     {:}     {:}     ({},{})'.format(p0, x1, y1, x1, y1))
+
+while True:
+    if p0 < 0:
+        p0 = p0 + 2*dy
+        x1 += 1
+        print(' {:}     {:}     {:}     ({},{})'.format(p0, x1, y1, x1, y1))
+    elif p0 >= 0:
+        p0 = p0 + (2*dy - 2*dx)
+        x1 += 1
+        y1 += 1
+        if p0 < 0:
+            print('{:}     {:}     {:}     ({},{})'.format(p0, x1, y1, x1, y1))
+        else:
+            print(' {:}     {:}     {:}     ({},{})'.format(p0, x1, y1, x1, y1))
+    if x1 == x2:
+        break
