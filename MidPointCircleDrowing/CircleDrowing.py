78b745766253cb277a162a0640d43e50f9eef150
Mithu12 <mosaddekalimithu123@gmail.com>
2020-12-08 23:18:57 +0600

    circle conflict removed removed

--- MidPointCircleDrowing/CircleDrowing.py
+++ MidPointCircleDrowing/CircleDrowing.py
@@ -1,10 +1,6 @@
 
 """ input r all time """
-<<<<<<< HEAD
-r = 6
-=======
 r = 11
->>>>>>> 6682067cbf8c0d484d1d4a3d58bf9e097a77a813
 if isinstance(r, float):
     p0 = (5/4) - r
 else:
@@ -19,13 +15,8 @@ k = 0
 #######Fixed######
 
 ###### Center value (if given then change) ######
-<<<<<<< HEAD
-cx = 2
-cy = 9
-=======
 cx = -5
 cy = 7
->>>>>>> 6682067cbf8c0d484d1d4a3d58bf9e097a77a813
 
 
 print('=========== Co-ords Values============')
